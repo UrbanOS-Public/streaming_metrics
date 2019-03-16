47febb3d44d72d05fe8409537d003f9a9b4d827c
Johnson Denen <johnson.denen@gmail.com>
2019-03-16 11:44:01 -0400

    Use ex_aws_cloudwatch v2.0.4 (#12)
    - Must use a hex artifact to publish this package
    - The code we needed was pulled into v2.0.4
    - Bump streaming_metrics version to 2.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StreamingMetrics.MixProject do
   def project do
     [
       app: :streaming_metrics,
-      version: "2.1.2",
+      version: "2.1.3",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -27,9 +27,7 @@ defmodule StreamingMetrics.MixProject do
     [
       {:httpoison, "~> 1.5.0"},
       {:mock, "~> 0.3.1", only: :test, runtime: false},
-      # v2.0.3 + metric timestamp fix; HEAD of master on 2018-08-11
-      {:ex_aws_cloudwatch,
-       github: "ex-aws/ex_aws_cloudwatch", sha: "258c0c2a81acf6acf68439d2e9c4cfdf8dec10eb"},
+      {:ex_aws_cloudwatch, "~> 2.0.4"},
       {:ex_aws, "~> 2.0.0"},
       {:prometheus_ex, "~> 3.0"},
       {:credo, "~> 0.10", only: [:dev, :test], runtime: false}
