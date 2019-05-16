948c4bdcc8c4bc6768a26e50b11a233fd38c7d64
badams-scos <34457752+badams-scos@users.noreply.github.com>
2019-05-16 07:40:47 -0400

    SMRT-1210 Bump Version (#17)
    co-authored-by: ManApart <akucera@pillartechnology.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StreamingMetrics.MixProject do
   def project do
     [
       app: :streaming_metrics,
-      version: "2.1.4",
+      version: "2.1.5",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
